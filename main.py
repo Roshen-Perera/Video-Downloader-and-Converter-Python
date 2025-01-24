4546efc2d8c4fe08a34fced286a953c249b3786e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-01-24 18:36:54 +0530

    added gui with tkinter and customtkinter

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-
+import tkinter
+import customtkinter
 from yt_dlp import YoutubeDL
 
-def download_video(url, quality="best", output_path="downloads"):
-    """Download video or audio with the selected quality."""
+def download_video():
 
     # Path to FFmpeg in the dependencies folder
-    ffmpeg_path = "dependencies\\windows\\ffmpeg.exe" 
+    ffmpeg_path = "D:\Python Projects\Video Downloader and Converter\\backend\dependencies\\ffmpeg.exe" 
+    
 
     if quality == "best":
         format_option = "bestvideo+bestaudio/best"
@@ -23,28 +24,59 @@ def download_video(url, quality="best", output_path="downloads"):
     # Base options
     ydl_opts = {
         'format': format_option,
-        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+        'outtmpl': f'{"downloads"}/%(title)s.%(ext)s',
         'ffmpeg_location': ffmpeg_path,  # Set path to FFmpeg
     }
 
     # Add MP3 conversion for audio downloads
     if quality == "audio":
-        audioQuality = input("Enter the quality of the audio: ")
+        # audioQuality = input("Enter the quality of the audio: ")
         ydl_opts.update({
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
-                'preferredquality': audioQuality,
+                'preferredquality': "192",
             }]
         })
     else:
         ydl_opts['merge_output_format'] = 'mp4'   # Only apply for video downloads
 
     with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+        ydl.download([urlVar.get()])
+        print("Download complete!")
 
-if __name__ == "__main__":
-    video_url = input("Enter YouTube video URL: ")
-    quality = input("Enter quality (best, 1080p, 720p, 480p, audio): ").strip().lower()
-    download_video(video_url, quality)
-    print("Download complete!")
+
+# Set appearance mode and default color theme
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+# Create the main window
+app = customtkinter.CTk()
+app.title("YouTube Downloader")
+app.geometry("800x600")
+
+# Create the widgets
+
+# URL Label
+urlLabel = customtkinter.CTkLabel(app, text="Enter YouTube video URL:")
+urlLabel.pack(padx=10, pady=20)
+
+# URL Input field
+urlVar = tkinter.StringVar()
+urlInput = customtkinter.CTkEntry(app, width=350, height=40, textvariable=urlVar)
+urlInput.pack()
+
+# Quality Label
+qualityLabel = customtkinter.CTkLabel(app, text="Enter the quality:")
+qualityLabel.pack(padx=10, pady=20)
+
+# Quality Input field
+quality = tkinter.StringVar()
+qualityInput = customtkinter.CTkEntry(app , textvariable=quality)
+qualityInput.pack()
+
+download_button = customtkinter.CTkButton(app, text="Download", command=download_video)
+download_button.pack(pady=20)
+ 
+# Run the main loop
+app.mainloop() 
